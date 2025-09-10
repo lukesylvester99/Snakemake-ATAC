3ae7e4002d36e5f941da866ca46f32156f37d554
Luke Sylvester <145699572+lukesylvester99@users.noreply.github.com>
2025-09-10 12:56:49 -0700

    removed extra R script for seurat object creation; added conda environment to snakefile rule;

--- scripts/install_signac_if_needed.R
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env Rscript
-if (!requireNamespace("Signac", quietly = TRUE)) {
-  if (!requireNamespace("remotes", quietly = TRUE)) {
-    install.packages("remotes", repos = "https://cloud.r-project.org")
-  }
-  remotes::install_github("timoast/signac", upgrade = "never", dependencies = FALSE)
-}
